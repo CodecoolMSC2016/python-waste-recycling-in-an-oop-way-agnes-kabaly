13838dd12d944fba3fbbb74379eaef7c8c65289a
agnes-kabaly <alicet.woods@gmail.com>
2017-01-16 00:51:02 +0100

    ready

--- dustbin.py
+++ dustbin.py
@@ -5,4 +5,29 @@ from dustbin_content_error import DustbinContentError
 
 
 class Dustbin:
-    pass
+    def __init__(self, color):
+        self.color = color
+        self.paper_content = []
+        self.plastic_content = []
+        self.house_waste_content = []
+
+    def throw_out_garbage(self, garbage):
+        if type(garbage) == PlasticGarbage:
+            if garbage.is_clean == True:
+                self.plastic_content.append(garbage)
+            else:
+                raise DustbinContentError
+        elif type(garbage) == PaperGarbage:
+            if garbage.is_squeezed == True:
+                self.paper_content.append(garbage)
+            else:
+                raise DustbinContentError
+        elif type(garbage) == Garbage:
+            self.house_waste_content.append(garbage)
+        else:
+            raise DustbinContentError
+
+    def empty_contents(self):
+        self.paper_content = []
+        self.plastic_content = []
+        self.house_waste_content = []
